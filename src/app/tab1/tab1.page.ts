7680ac9e53592b7778068a6b669d7a68f84f33ce
Felixmau360 <mfcolombo@gmail.com>
2024-11-22 00:25:26 -0300

    v.0.10.20.1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +8,37 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  isAlertOpen = false;
+  alertButtons = ['Ok'];
+
+  setOpen(isOpen: boolean) {
+    this.isAlertOpen = isOpen;
+  }
+
   constructor() {}
 
+
+  desempetanol:number = NaN;
+  desempgas:number = NaN;
+  precogas:number = NaN;
+  precoetanol:number = NaN;
+  consumoetanol:number = NaN;
+  consumogas:number = NaN;
+  resultado:string ="";
+ 
+ funcao(){
+
+   this.consumoetanol = this.precoetanol / this.desempetanol;
+   this.consumogas = this.precogas / this.desempgas;
+ 
+   if (this.consumoetanol < this.consumogas) {
+       this.resultado = "Abasteça com Etanol";
+     } 
+   else if (this.consumoetanol > this.consumogas) {
+       this.resultado = "Abasteça com Gasolina";
+     } 
+   else {
+       this.resultado = "Use o Combustível de sua preferência.";
+     }
+   }
 }
